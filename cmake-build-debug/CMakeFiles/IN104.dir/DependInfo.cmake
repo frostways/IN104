2034ebdf5e1bcff12e069ff2f77084f8248f1d0a
Xu-kexin <596059345@qq.com>
2022-03-30 03:38:51 +0800

    new wave.c with segmentation faut

---
+++ cmake-build-debug/CMakeFiles/IN104.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/c/Users/59605/Desktop/ensta/im104ok/IN104/stft.c" "/mnt/c/Users/59605/Desktop/ensta/im104ok/IN104/cmake-build-debug/CMakeFiles/IN104.dir/stft.c.o"
+  "/mnt/c/Users/59605/Desktop/ensta/im104ok/IN104/wave.c" "/mnt/c/Users/59605/Desktop/ensta/im104ok/IN104/cmake-build-debug/CMakeFiles/IN104.dir/wave.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/IN104.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/IN104.dir/stft.c.o"
+  "CMakeFiles/IN104.dir/wave.c.o"
+  "IN104"
+  "IN104.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/IN104.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
