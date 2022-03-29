2a54703156198bc0384cbe5fb3ef2a2e0451663a
Xu-kexin <596059345@qq.com>
2022-03-29 23:14:04 +0800

    enleve un fichier test

--- audio-read/wave.c
+++ audio-read/wave.c
@@ -22,7 +22,7 @@ char* seconds_to_time(float seconds);
  char *filename;
  struct HEADER header;
 
-long readWave(char * path, int* vect) {
+int main(int argc, char **argv) {
 
  filename = (char*) malloc(sizeof(char) * 1024);
  if (filename == NULL) {
@@ -37,9 +37,13 @@ long readWave(char * path, int* vect) {
     strcpy(filename, cwd);
 
     // get filename from command line
-
+    if (argc < 2) {
+      printf("No wave file specifiedn");
+      return;
+    }
+    
     strcat(filename, "/");
-    strcat(filename, path);
+    strcat(filename, argv[1]);
     printf("%sn", filename);
  }
 
@@ -154,8 +158,6 @@ long readWave(char * path, int* vect) {
  long num_samples = (8 * header.data_size) / (header.channels * header.bits_per_sample);
  printf("Number of samples:%lu n", num_samples);
 
- vect  = malloc(num_samples * sizeof (int));
-
  long size_of_each_sample = (header.channels * header.bits_per_sample) / 8;
  printf("Size of each sample:%ld bytesn", size_of_each_sample);
 
@@ -204,7 +206,7 @@ long readWave(char * path, int* vect) {
             }					
 
             printf("nn.Valid range for data values : %ld to %ld n", low_limit, high_limit);
-            for (i =1; i <= num_samples; i++) {
+            for (i =1; i <= 10; i++) {
                 printf("==========Sample %ld / %ld=============n", i, num_samples);
                 read = fread(data_buffer, sizeof(data_buffer), 1, ptr);
                 if (read == 1) {
@@ -233,7 +235,6 @@ long readWave(char * path, int* vect) {
 
                         offset += bytes_in_each_channel;		
                         printf("%d ", data_in_channel);
-                        vect[i]  = data_in_channel;
 
                         // check if value was in range
                         if (data_in_channel < low_limit || data_in_channel > high_limit)
@@ -261,7 +262,7 @@ long readWave(char * path, int* vect) {
 
   // cleanup before quitting
  free(filename);
- return num_samples;
+ return 0;
 
 }
 
